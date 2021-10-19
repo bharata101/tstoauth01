89ca9714b7bb031a1eaa0af224121ebbf17664f0
bharata101 <galuhdipa@MSI.localdomain>
2021-10-20 06:24:54 +0700

    redirecting

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@ import json
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
-
+from starlette.responses import RedirectResponse
 from passlib.context import CryptContext
 
 from pydantic import BaseModel
