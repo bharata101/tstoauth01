f938f04057fe81bbe5a9c58a4c98c65a1c3aebcc
bharata101 <galuhdipa@MSI.localdomain>
2021-10-20 07:54:34 +0700

    uvicorn main

--- hash.py
+++ hash.py
@@ -1,13 +1,4 @@
-from datetime import datetime, timedelta
-from typing import Optional
-import json
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-
 from passlib.context import CryptContext
-
-from pydantic import BaseModel
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -22,4 +13,3 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 print(get_password_hash("asdf"))
-print(verify_password('asdf',get_password_hash('asdf')))
